045b4114363450b5591bb0706c430baa42a6c85c
Hoang Kha <chauhoangkha442@gmail.com>
2026-03-06 08:51:13 +0700

    fix router routing

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
